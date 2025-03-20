71db1d61e304c8df741be87ae8083ebb4891cf47
Søren István Adorján Dyhr <siadyhr@gmail.com>
2025-03-20 19:58:30 +0100

    Add function to plot level sets in p plane, making a grid for varying q
    Signed-off-by: Søren István Adorján Dyhr <siadyhr@gmail.com>

--- 3BP/level-sets/plots.py
+++ 3BP/level-sets/plots.py
@@ -78,3 +78,39 @@ def qgrid_level_sets():
                 levels=np.linspace(-2, 0, 20)
             )
             axs[i,j].set_title("$p = (%s, %s)$" % (P1[0,0,i,j], P2[0,0,i,j]))
+
+def pgrid_level_sets():
+    """Create a grid of
+    (p_1, p_2) plots showing
+    level sets of H for
+    different values of p
+    (the rows/cols of the
+    plots vary the p value)
+    """
+    N = 100
+    n_plots = 5
+    mu = 0.25
+    qmin = -2**0.5
+    qmax = -qmin
+    pmin = -1
+    pmax = 1
+
+    q1s = np.linspace(qmin, qmax, n_plots)
+    q2s = np.linspace(qmin, qmax, n_plots)
+    p1s = np.linspace(pmin, pmax, N)
+    p2s = np.linspace(pmin, pmax, N)
+
+    Q1, Q2, P1, P2 = np.meshgrid(q1s, q2s, p1s, p2s)
+    H = Hamiltonian(Q1, Q2, P1, P2, mu=mu)
+
+    fig, axs = plt.subplots(n_plots, n_plots)
+    fig.suptitle("Level sets of the Hamiltonian $H_{%s}$" % mu)
+    for i in range(n_plots):
+        for j in range(n_plots):
+            axs[i,j].contour(
+                P1[i,j,:,:],
+                P2[i,j,:,:],
+                H[i,j,:,:],
+                levels=np.linspace(-2, 0, 20)
+            )
+            axs[i,j].set_title("$q = (%s, %s)$" % (Q1[i,j,0,0], Q2[i,j,0,0]))
